b64e0426204ef6e0ec01371fa26666f8cf920861
shaunmcgann <smcgann20@gmail.com>
2017-07-03 16:39:45 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1HviS80YuLPgtcet1-hS-fgR2xEj3Ija4wZtC7BK6Rxg/edit#gid=0';
